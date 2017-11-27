dc9656ce4c8a069dc1a38a1dee3c011b8906d09e
duilmer <34013638+duilmer@users.noreply.github.com>
2017-11-26 22:11:22 -0400

    Update ProyectoPrueba.java

--- ProyectoPrueba/src/proyectoprueba/ProyectoPrueba.java
+++ ProyectoPrueba/src/proyectoprueba/ProyectoPrueba.java
@@ -15,7 +15,11 @@ public class ProyectoPrueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         // TODO code application logic here
+        System.out.println("Prueba");
+        System.out.println(System.getProperty("java.version"));
+        System.out.println("Final");
     }
     
 }
